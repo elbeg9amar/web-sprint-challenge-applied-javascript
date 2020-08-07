23d2f9a856027cd3b182c3494911f66a380446b4
Amarbayasgalan elbeg <elbeg9amar@yahoo.com>
2020-08-07 11:45:00 -0400

    completed header

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,30 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const headerDiv = document.createElement('div');
+    const dateSpan = document.createElement('span');
+    const title = document.createElement('h1');
+    const tempSpan = document.createElement('span');
+
+    headerDiv.classList.add('header');
+    dateSpan.classList.add('date');
+    tempSpan.classList.add('temp');
+
+    dateSpan.textContent = 'March 28, 2020';
+    title.textContent = 'Lambda Times';
+    tempSpan.textContent = '98°';
+
+    headerDiv.appendChild(dateSpan);
+    headerDiv.appendChild(title);
+    headerDiv.appendChild(tempSpan);
+    
+    return headerDiv
+}
+
+   const headerBox = document.querySelector('.header-container')
+   headerBox.appendChild(Header());
+    
+    
+
+    
